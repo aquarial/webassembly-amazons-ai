6f7e843774bcdb3ba226e0e465f67844d1803486
Karl <37496339+aquarial@users.noreply.github.com>
2020-03-21 15:17:18 -0500

    Add simple checkerboard

--- website/index.js
+++ website/index.js
@@ -7,11 +7,34 @@ import { GameBoard, Player } from "./board.js";
 /** @type {((dt: number, totaltime: number) => void)[]} */
 let animations = [];
 
-let gameboard = new GameBoard(8, 8);
-gameboard.addPlayer(new Player(3, 3, "red"));
-gameboard.addPlayer(new Player(3, 6, "red"));
-gameboard.addPlayer(new Player(6, 3, "blue"));
-gameboard.addPlayer(new Player(6, 6, "blue"));
+
+{
+
+  /** @type {HTMLCanvasElement} */
+  let canvas = (document.getElementById("thecanvas"));
+  let c2d = canvas.getContext("2d");
+
+  let gameboard = new GameBoard(8, 8);
+  gameboard.addPlayer(new Player(3, 3, "red"));
+  gameboard.addPlayer(new Player(3, 6, "red"));
+  gameboard.addPlayer(new Player(6, 3, "blue"));
+  gameboard.addPlayer(new Player(6, 6, "blue"));
+
+  let tilesize = canvas.width / gameboard.width;
+  let checker_colors = ["brown", "gray"]
+
+  for (let y = 1; y <= gameboard.height; y++) {
+    for (let x = 1; x <= gameboard.width; x++) {
+      c2d.fillStyle = checker_colors[(x + y) % 2]
+      c2d.fillRect((x - 1) * tilesize, (y - 1) * tilesize, tilesize, tilesize)
+    }
+  }
+
+  animations.push((dt, totaltime) => {
+
+  })
+}
+
 
 {
   let marker = 0;
