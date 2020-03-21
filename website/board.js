135b354c47d20e78495f10ba70926b65d5891251
Karl <37496339+aquarial@users.noreply.github.com>
2020-03-21 15:08:30 -0500

    Add player class

--- website/board.js
+++ website/board.js
@@ -2,8 +2,41 @@
 // @ts-check
 
 
+export class Player {
+    /**
+     * @param {number} x
+     * @param {number} y
+     * @param {string} team
+     */
+    constructor(y, x, team) {
+        this.x = x
+        this.y = y
+        this.team = team
+    }
+
+    pos() {
+        return [this.y, this.x]
+    }
+}
+
 export class GameBoard {
-    constructor() {
-        this.board = new Set()
+    /**
+     *
+     * Board goes from (1,1) to (height,width) inclusive
+     * @param {number} width
+     * @param {number} height
+     */
+    constructor(width, height) {
+        this.width = width
+        this.height = height
+        /** @type {Map<Number[],Boolean|Player>} */
+        this.blocked = new Map()
+    }
+
+    /**
+     * @param {Player} player
+     */
+    addPlayer(player) {
+        this.blocked.set(player.pos(), player)
     }
 }
