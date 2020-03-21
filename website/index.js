9d77cabcf0da22c25b2d3ff2dc0988abb6fd689e
Karl <37496339+aquarial@users.noreply.github.com>
2020-03-21 14:30:12 -0500

    Add canvas & counter in animation loop

--- website/index.js
+++ website/index.js
@@ -1,3 +1,35 @@
+// @ts-check
+/* jshint -W069, esversion:6 */
+
 import * as wasm from "amazons-ai-webassembly";
 
-wasm.greet();
+let animations = [];
+
+let canvas = /** @type {HTMLCanvasElement} */ (document.getElementById("thecanvas"));
+let c2d = canvas.getContext("2d");
+
+let time = 0;
+let marker = 0;
+/** @param {number} dt */
+animations.push(dt => {
+  time += dt;
+  if (time > marker) {
+    console.log(marker, "seconds");
+    marker += 5;
+  }
+});
+
+
+let previous = performance.now();
+function animLoop() {
+  let next = performance.now();
+  let deltaTime = (next - previous) / 1000.0;
+  previous = next;
+
+  animations.forEach(it => {
+    it(deltaTime);
+  });
+
+  window.requestAnimationFrame(animLoop);
+}
+animLoop();
