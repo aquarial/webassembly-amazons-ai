cc1fcc15a3a21795b199d6b3b2788b6cb58aa340
Karl <37496339+aquarial@users.noreply.github.com>
2020-03-22 12:29:57 -0500

    Add drawing code

--- website/drawstate.js
+++ website/drawstate.js
@@ -17,6 +17,32 @@ export class DrawState {
   }
 
 
+  team_color(team_name, faded=false) {
+    if (team_name == "block") {
+      if (faded) {
+        return '#30303066'
+      } else {
+        return '#303030'
+      }
+
+    }
+    if (team_name == "red") {
+      if (faded) {
+        return "#ff000066"
+      } else {
+        return "#ff0000"
+      }
+    } 
+    
+    if (team_name == "blue") {
+      if (faded) {
+        return "#0000ff66"
+      } else {
+        return "#0000ff"
+      }
+    }
+  }
+
   /**
    * @param {CanvasRenderingContext2D} c2d
    * @param {GameBoard} gameboard
@@ -31,27 +57,108 @@ export class DrawState {
         c2d.fillRect((x - 1) * tilesize, (y - 1) * tilesize, tilesize, tilesize)
 
         let at = gameboard.atYX(y, x);
-        if (at instanceof Player) {
-          if (at == this.piece) {
+
+        if (this.piece == null) { // selecting a piece
+
+          if (at instanceof Player) {
+            c2d.beginPath();
+            c2d.fillStyle = "white"
+            c2d.ellipse((x - 1 + 0.5) * tilesize, (y - 1 + 0.5) * tilesize,
+              (tilesize * 1.1) * 0.3, (tilesize * 1.1) * 0.3, 0, 0, 360);
+            c2d.fill();
+            c2d.fillStyle = this.team_color(at.team)
+            c2d.beginPath();
+            c2d.ellipse((x - 1 + 0.5) * tilesize, (y - 1 + 0.5) * tilesize,
+              tilesize * 0.3, tilesize * 0.3, 0, 0, 360);
+            c2d.fill();
+          } else if (at != undefined) {
+            c2d.fillStyle = this.team_color("block")
+            c2d.fillRect((x - 1 + 0.2) * tilesize, (y - 1 + 0.2) * tilesize, tilesize * 0.6, tilesize * 0.6)
+          }
+
+        } else if (this.move == null) { // selecting a move
+
+          if (at === this.piece) {
             c2d.beginPath();
             c2d.fillStyle = "gray"
             c2d.ellipse((x - 1 + 0.5) * tilesize, (y - 1 + 0.5) * tilesize,
-              (tilesize * 1.4) * 0.3, (tilesize * 1.4) * 0.3, 0, 0, 360);
+              (tilesize ) * 0.3, (tilesize) * 0.3, 0, 0, 360);
             c2d.fill();
           }
-          c2d.beginPath();
-          c2d.fillStyle = "white"
-          c2d.ellipse((x - 1 + 0.5) * tilesize, (y - 1 + 0.5) * tilesize,
-            (tilesize * 1.1) * 0.3, (tilesize * 1.1) * 0.3, 0, 0, 360);
-          c2d.fill();
-          c2d.fillStyle = at.team
-          c2d.beginPath();
-          c2d.ellipse((x - 1 + 0.5) * tilesize, (y - 1 + 0.5) * tilesize,
-            tilesize * 0.3, tilesize * 0.3, 0, 0, 360);
-          c2d.fill();
-        } else if (at != undefined) {
-          c2d.fillStyle = '#303030'
-          c2d.fillRect((x - 1 + 0.2) * tilesize, (y - 1 + 0.2) * tilesize, tilesize * 0.6, tilesize * 0.6)
+
+          if (at == undefined && this.mouse_pos.y == y && this.mouse_pos.x == x) {
+            if (gameboard.openLineTo(this.piece.pos, this.mouse_pos)) {
+              c2d.fillStyle = this.team_color(this.piece.team, true)
+              c2d.beginPath();
+              c2d.ellipse((x - 1 + 0.5) * tilesize, (y - 1 + 0.5) * tilesize,
+                tilesize * 0.3, tilesize * 0.3, 0, 0, 360);
+              c2d.fill();
+            }
+          }
+
+          if (at instanceof Player) {
+            if (at != this.piece) {
+              c2d.beginPath();
+              c2d.fillStyle = "white"
+              c2d.ellipse((x - 1 + 0.5) * tilesize, (y - 1 + 0.5) * tilesize,
+                (tilesize * 1.1) * 0.3, (tilesize * 1.1) * 0.3, 0, 0, 360);
+              c2d.fill();
+              c2d.fillStyle = at.team;
+              c2d.beginPath();
+              c2d.ellipse((x - 1 + 0.5) * tilesize, (y - 1 + 0.5) * tilesize,
+                tilesize * 0.3, tilesize * 0.3, 0, 0, 360);
+              c2d.fill();
+            }
+          } else if (at != undefined) {
+            c2d.fillStyle = this.team_color("block")
+            c2d.fillRect((x - 1 + 0.2) * tilesize, (y - 1 + 0.2) * tilesize, tilesize * 0.6, tilesize * 0.6)
+          }
+
+
+        } else { // selecting stone
+
+          if (at === this.piece && !(y == this.mouse_pos.y && x == this.mouse_pos.x)) {
+            c2d.beginPath();
+            c2d.fillStyle = "gray"
+            c2d.ellipse((x - 1 + 0.5) * tilesize, (y - 1 + 0.5) * tilesize,
+              (tilesize ) * 0.3, (tilesize ) * 0.3, 0, 0, 360);
+            c2d.fill();
+          }
+
+          if (at instanceof Player) {
+            if (at != this.piece) {
+              c2d.beginPath();
+              c2d.fillStyle = "white"
+              c2d.ellipse((x - 1 + 0.5) * tilesize, (y - 1 + 0.5) * tilesize,
+                (tilesize * 1.1) * 0.3, (tilesize * 1.1) * 0.3, 0, 0, 360);
+              c2d.fill();
+              c2d.fillStyle = this.team_color(at.team)
+              c2d.beginPath();
+              c2d.ellipse((x - 1 + 0.5) * tilesize, (y - 1 + 0.5) * tilesize,
+                tilesize * 0.3, tilesize * 0.3, 0, 0, 360);
+              c2d.fill();
+            }
+          } else if (at != undefined) {
+            c2d.fillStyle = this.team_color("block")
+            c2d.fillRect((x - 1 + 0.2) * tilesize, (y - 1 + 0.2) * tilesize, tilesize * 0.6, tilesize * 0.6)
+          } else if (y == this.move.y && x == this.move.x) {
+            c2d.fillStyle = this.team_color(this.piece.team)
+            c2d.beginPath();
+            c2d.ellipse((x - 1 + 0.5) * tilesize, (y - 1 + 0.5) * tilesize,
+              tilesize * 0.3, tilesize * 0.3, 0, 0, 360);
+            c2d.fill();
+          }
+
+          gameboard.blocked.set(this.piece.pos.str(), undefined);
+          if (y == this.mouse_pos.y && x == this.mouse_pos.x) {
+            if (gameboard.openLineTo(this.move, this.mouse_pos)) {
+              c2d.fillStyle = this.team_color("block", true)
+              c2d.fillRect((x - 1 + 0.2) * tilesize, (y - 1 + 0.2) * tilesize, tilesize * 0.6, tilesize * 0.6)
+            }
+          }
+          gameboard.blocked.set(this.piece.pos.str(), this.piece);
+
+
         }
       }
     }
