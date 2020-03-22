ca742e7c51ff0a80931f9d538d2d1364381b8793
Karl <37496339+aquarial@users.noreply.github.com>
2020-03-22 01:40:52 -0500

    4 spaces --> 2 spaces

--- website/drawstate.js
+++ website/drawstate.js
@@ -6,55 +6,55 @@ import { Player, Pos, GameBoard } from "./game";
 
 
 export class DrawState {
-    constructor() {
-        this.tilex = -1;
-        this.tiley = -1;
+  constructor() {
+    this.tilex = -1;
+    this.tiley = -1;
 
-        /** @type {Player} */
-        this.piece = null
+    /** @type {Player} */
+    this.piece = null
 
-        /** @type {Pos} */
-        this.move = null
-    }
+    /** @type {Pos} */
+    this.move = null
+  }
 
 
-    /**
+  /**
    * @param {CanvasRenderingContext2D} c2d
    * @param {GameBoard} gameboard
    * @param {number} tilesize
    */
-    drawTiles(c2d, gameboard, tilesize) {
-        let checker_colors = ["#eae8ea", "#c1c1c1"]
+  drawTiles(c2d, gameboard, tilesize) {
+    let checker_colors = ["#eae8ea", "#c1c1c1"]
 
-        for (let y = 1; y <= gameboard.height; y++) {
-            for (let x = 1; x <= gameboard.width; x++) {
-                c2d.fillStyle = checker_colors[(x + y) % 2]
-                c2d.fillRect((x - 1) * tilesize, (y - 1) * tilesize, tilesize, tilesize)
+    for (let y = 1; y <= gameboard.height; y++) {
+      for (let x = 1; x <= gameboard.width; x++) {
+        c2d.fillStyle = checker_colors[(x + y) % 2]
+        c2d.fillRect((x - 1) * tilesize, (y - 1) * tilesize, tilesize, tilesize)
 
-                let at = gameboard.atYX(y, x);
-                if (at instanceof Player) {
-                    if (at == this.piece) {
-                        c2d.beginPath();
-                        c2d.fillStyle = "gray"
-                        c2d.ellipse((x - 1 + 0.5) * tilesize, (y - 1 + 0.5) * tilesize,
-                            (tilesize * 1.4) * 0.3, (tilesize * 1.4) * 0.3, 0, 0, 360);
-                        c2d.fill();
-                    }
-                    c2d.beginPath();
-                    c2d.fillStyle = "white"
-                    c2d.ellipse((x - 1 + 0.5) * tilesize, (y - 1 + 0.5) * tilesize,
-                        (tilesize * 1.1) * 0.3, (tilesize * 1.1) * 0.3, 0, 0, 360);
-                    c2d.fill();
-                    c2d.fillStyle = at.team
-                    c2d.beginPath();
-                    c2d.ellipse((x - 1 + 0.5) * tilesize, (y - 1 + 0.5) * tilesize,
-                        tilesize * 0.3, tilesize * 0.3, 0, 0, 360);
-                    c2d.fill();
-                } else if (at != undefined) {
-                    c2d.fillStyle = '#303030'
-                    c2d.fillRect((x - 1 + 0.3) * tilesize, (y - 1 + 0.3) * tilesize, tilesize * 0.4, tilesize * 0.4)
-                }
-            }
+        let at = gameboard.atYX(y, x);
+        if (at instanceof Player) {
+          if (at == this.piece) {
+            c2d.beginPath();
+            c2d.fillStyle = "gray"
+            c2d.ellipse((x - 1 + 0.5) * tilesize, (y - 1 + 0.5) * tilesize,
+              (tilesize * 1.4) * 0.3, (tilesize * 1.4) * 0.3, 0, 0, 360);
+            c2d.fill();
+          }
+          c2d.beginPath();
+          c2d.fillStyle = "white"
+          c2d.ellipse((x - 1 + 0.5) * tilesize, (y - 1 + 0.5) * tilesize,
+            (tilesize * 1.1) * 0.3, (tilesize * 1.1) * 0.3, 0, 0, 360);
+          c2d.fill();
+          c2d.fillStyle = at.team
+          c2d.beginPath();
+          c2d.ellipse((x - 1 + 0.5) * tilesize, (y - 1 + 0.5) * tilesize,
+            tilesize * 0.3, tilesize * 0.3, 0, 0, 360);
+          c2d.fill();
+        } else if (at != undefined) {
+          c2d.fillStyle = '#303030'
+          c2d.fillRect((x - 1 + 0.3) * tilesize, (y - 1 + 0.3) * tilesize, tilesize * 0.4, tilesize * 0.4)
         }
+      }
     }
+  }
 }
--- website/game.js
+++ website/game.js
@@ -1,171 +1,171 @@
-/*jshint esversion: 6 */
 // @ts-check
+/* jshint -W069, esversion:6 */
 
 
 export class Pos {
-    /**
-    * @param {number} x
-    * @param {number} y
-    */
-    constructor(y, x) {
-        this.x = x
-        this.y = y
-    }
+  /**
+   * @param {number} x
+   * @param {number} y
+   */
+  constructor(y, x) {
+    this.x = x
+    this.y = y
+  }
 
-    copy() {
-        return new Pos(this.y, this.x);
-    }
+  copy() {
+    return new Pos(this.y, this.x);
+  }
 
-    str() {
-        return `${this.y} ${this.x}`
-    }
+  str() {
+    return `${this.y} ${this.x}`
+  }
 }
 
 export class Player {
-    /**
-     * @param {Pos} pos
-     * @param {string} team
-     */
-    constructor(pos, team) {
-        this.pos = pos
-        this.team = team
-    }
+  /**
+   * @param {Pos} pos
+   * @param {string} team
+   */
+  constructor(pos, team) {
+    this.pos = pos
+    this.team = team
+  }
 }
 
 export class GameBoard {
-    /**
-     *
-     * Board goes from (1,1) to (height,width) inclusive
-     * @param {number} width
-     * @param {number} height
-     */
-    constructor(width, height) {
-        this.width = width
-        this.height = height
-        /** @type {Map<string,any|Player>} */
-        this.blocked = new Map()
+  /**
+   *
+   * Board goes from (1,1) to (height,width) inclusive
+   * @param {number} width
+   * @param {number} height
+   */
+  constructor(width, height) {
+    this.width = width
+    this.height = height
+    /** @type {Map<string,any|Player>} */
+    this.blocked = new Map()
+  }
+
+  /**
+   * @param {Player|Pos} p
+   */
+  atPos(p) {
+    if (p instanceof Player)
+      p = p.pos
+    return this.blocked.get(p.str())
+  }
+  /**
+   * @param {Number} y
+   * @param {Number} x
+   */
+  atYX(y, x) {
+    return this.blocked.get(`${y} ${x}`)
+  }
+
+  /**
+   * @param {Player|Pos} p0
+   * @param {Player|Pos} p1
+   */
+  openLineTo(p0, p1) {
+    if (p0 instanceof Player)
+      p0 = p0.pos
+    if (p1 instanceof Player)
+      p1 = p1.pos
+    //
+    let y0 = p0.y;
+    let x0 = p0.x;
+    let y1 = p1.y;
+    let x1 = p1.x;
+
+    if (y0 == y1 && x0 == x1) {
+      return false; // can't be same point
+    }
+    if (y0 != y1 && x0 != x1 && Math.abs(y1 - y0) != Math.abs(x1 - x0)) {
+      return false; // not a line
     }
 
-    /**
-     * @param {Player|Pos} p
-     */
-    atPos(p) {
-        if (p instanceof Player)
-            p = p.pos
-        return this.blocked.get(p.str())
-    }
-    /**
-     * @param {Number} y
-     * @param {Number} x
-     */
-    atYX(y, x) {
-        return this.blocked.get(`${y} ${x}`)
+    let distance = Math.max(Math.abs(y1 - y0), Math.abs(x1 - x0));
+
+    for (let dt = 1; dt <= distance; dt++) {
+      if (this.atYX(y0 + dt * Math.sign(y1 - y0), x0 + dt * Math.sign(x1 - x0))) {
+        return false;
+      }
     }
 
-    /**
-     * @param {Player|Pos} p0
-     * @param {Player|Pos} p1
-     */
-    openLineTo(p0, p1) {
-        if (p0 instanceof Player)
-            p0 = p0.pos
-        if (p1 instanceof Player)
-            p1 = p1.pos
-        //
-        let y0 = p0.y;
-        let x0 = p0.x;
-        let y1 = p1.y;
-        let x1 = p1.x;
+    return true;
+  }
 
-        if (y0 == y1 && x0 == x1) {
-            return false; // can't be same point
-        }
-        if (y0 != y1 && x0 != x1 && Math.abs(y1 - y0) != Math.abs(x1 - x0)) {
-            return false; // not a line
-        }
+  /**
+   * @param {Player} player
+   */
+  addPlayer(player) {
+    this.blocked.set(player.pos.str(), player)
+  }
 
-        let distance = Math.max(Math.abs(y1 - y0), Math.abs(x1 - x0));
-
-        for (let dt = 1; dt <= distance; dt++) {
-            if (this.atYX(y0 + dt * Math.sign(y1 - y0), x0 + dt * Math.sign(x1 - x0))) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    /**
-     * @param {Player} player
-     */
-    addPlayer(player) {
-        this.blocked.set(player.pos.str(), player)
-    }
-
-    /**
-     * @param {Player} player
-     * @param {Pos} move
-     * @param {Pos} stone
-     */
-    makePlayerMove(player, move, stone) {
-        this.blocked.set(player.pos.str(), undefined)
-        this.blocked.set(move.str(), player)
-        player.pos.y = move.y
-        player.pos.x = move.x
-        this.blocked.set(stone.str(), 1)
-    }
+  /**
+   * @param {Player} player
+   * @param {Pos} move
+   * @param {Pos} stone
+   */
+  makePlayerMove(player, move, stone) {
+    this.blocked.set(player.pos.str(), undefined)
+    this.blocked.set(move.str(), player)
+    player.pos.y = move.y
+    player.pos.x = move.x
+    this.blocked.set(stone.str(), 1)
+  }
 }
 
 class Move {
-    constructor(player, move, stone, ai) {
-        this.player = player.copy();
-        this.move = move.copy();
-        this.stone = stone.copy();
-        this.ai = ai;
-    }
+  constructor(player, move, stone, ai) {
+    this.player = player.copy();
+    this.move = move.copy();
+    this.stone = stone.copy();
+    this.ai = ai;
+  }
 }
 
 export class GameState {
 
-    constructor() {
-        /** @type {Move[]} */
-        this.history = []
+  constructor() {
+    /** @type {Move[]} */
+    this.history = []
+    this.next_to_go = "red"
+  }
+
+  undoMove(gameboard) {
+    let v = this.history.pop()
+
+    if (v != undefined) {
+      if (this.next_to_go == "red") {
+        this.next_to_go = 'blue'
+      } else {
         this.next_to_go = "red"
+      }
+      gameboard.blocked.set(v.stone.str(), undefined)
+
+      let player = gameboard.atPos(v.move);
+      gameboard.blocked.set(v.player.str(), player)
+      gameboard.blocked.set(v.move.str(), undefined)
+      player.pos.y = v.player.y
+      player.pos.x = v.player.x
+      if (v.ai) {
+        this.undoMove(gameboard);
+      }
     }
+  }
 
-    undoMove(gameboard) {
-        let v = this.history.pop()
-
-        if (v != undefined) {
-            if (this.next_to_go == "red") {
-                this.next_to_go = 'blue'
-            } else {
-                this.next_to_go = "red"
-            }
-            gameboard.blocked.set(v.stone.str(), undefined)
-
-            let player = gameboard.atPos(v.move);
-            gameboard.blocked.set(v.player.str(), player)
-            gameboard.blocked.set(v.move.str(), undefined)
-            player.pos.y = v.player.y
-            player.pos.x = v.player.x
-            if (v.ai) {
-                this.undoMove(gameboard);
-            }
-        }
+  /**
+   * @param {Player} player
+   * @param {Pos} move
+   * @param {Pos} stone
+   */
+  addMove(player, move, stone) {
+    this.history.push(new Move(player.pos, move, stone, false))
+    if (this.next_to_go == "red") {
+      this.next_to_go = 'blue'
+    } else {
+      this.next_to_go = "red"
     }
-
-    /**
-     * @param {Player} player
-     * @param {Pos} move
-     * @param {Pos} stone
-     */
-    addMove(player, move, stone) {
-        this.history.push(new Move(player.pos, move, stone, false))
-        if (this.next_to_go == "red") {
-            this.next_to_go = 'blue'
-        } else {
-            this.next_to_go = "red"
-        }
-    }
+  }
 }
