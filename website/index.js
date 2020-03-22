c4e8df97935e30d0af483bcac708cdcf4e1ed133
Karl <37496339+aquarial@users.noreply.github.com>
2020-03-22 15:16:39 -0500

    Make the move in javascript

--- website/index.js
+++ website/index.js
@@ -55,6 +55,17 @@ let animations = [];
     if (board.is_valid()) {
       let r = wasm.compute_ai_move(board)
       console.log(r.piece_y, r.piece_x, r.move_y, r.move_x, r.stone_y, r.stone_x)
+      if (r.piece_y > 0 && r.piece_x > 0 &&
+        r.move_y > 0 && r.move_x > 0 &&
+        r.stone_y > 0 && r.stone_x > 0) {
+        let p0 = new Pos(r.piece_y, r.piece_x)
+        let p1 = new Pos(r.move_y, r.move_x)
+        let p2 = new Pos(r.stone_y, r.stone_x)
+        gamestate.addMove(gameboard.atPos(p0), p1, p2)
+        gameboard.makePlayerMove(gameboard.atPos(p0), p1, p2)
+        drawstate.piece = null
+        drawstate.move = null;
+      }
       r.free();
     } else {
       alert("Error: invalid board??")
