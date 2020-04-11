3d2bd4d2d8b5baf7b12d35c59296a6de52ec1164
Karl <37496339+aquarial@users.noreply.github.com>
2020-04-10 22:35:57 -0500

    Switch to min_max

--- rust/src/solver/algo.rs
+++ rust/src/solver/algo.rs
@@ -4,53 +4,6 @@ use smallvec::SmallVec;
 use std::collections::VecDeque;
 
 
-pub fn smart_min_max(cache: &mut DistState, board: &Board, team: Team) -> (Option<CompactMove>, i64) {
-  let mut local_board = board.clone();
-
-  let mut moves = Vec::<(CompactMove, i64)>::new();
-
-  for m in board.successors(team) {
-    local_board.apply_move(&m);
-    let eval = -evaluate_by_queen_bfs_distance(&local_board, team.other(), cache);
-    local_board.un_apply_move(&m);
-
-    let mut other = (-1, eval);
-    for (ix, (cm, cv)) in moves.iter().enumerate() {
-      if m.old_pos == cm.old_pos && m.new_pos == cm.new_pos {
-        other = (ix as i64, *cv);
-        break;
-      }
-    }
-    if other.0 == -1 {
-      moves.push((m, eval));
-      moves.truncate(4);
-    } else if eval > other.1 {
-      moves[other.0 as usize] = (m, eval);
-    }
-  }
-
-  let mut best: Option<CompactMove> = None;
-  let mut score: i64 = i64::min_value() + 1;
-
-  for (b, _) in moves {
-    local_board.apply_move(&b);
-    let (_, resp_score) = min_max(cache, &local_board, team.other(), 2);
-    local_board.un_apply_move(&b);
-
-    if score < -resp_score {
-      score = -resp_score;
-      best = Some(b);
-    }
-  }
-
-  match best {
-    None => min_max(cache, board, team, 1),
-    _ => (best, score)
-  }
-}
-
-
-
 pub fn min_max(cache: &mut DistState, board: &Board, team: Team, depth: i32) -> (Option<CompactMove>, i64) {
   let mut local_board = board.clone();
 
