95ef1da6d7429cdecbe9dbe44dd2637ef02cb2a0
Karl <37496339+aquarial@users.noreply.github.com>
2020-05-16 17:46:32 -0500

    Amazons will store Board/Move instead of Compact versions

--- rust/src/solver/mod.rs
+++ rust/src/solver/mod.rs
@@ -1,26 +1,29 @@
 pub mod algo;
 pub mod board;
+pub mod compact_board;
 
 use board::*;
+use compact_board::*;
 
 enum HistoryMove {
-  NewGame(CompactBoard),
-  Move(CompactMove),
+  NewGame(Board),
+  Move(Move),
 }
 
 /// Data structures for amazon simulation,
 /// history-tracking, and AI.
 pub struct Amazons {
-  current: CompactBoard,
+  current: Board,
   history: Vec<HistoryMove>,
   cache: DistState,
 }
 
 impl Amazons {
-  pub fn from_board(board: CompactBoard) -> Amazons {
+  pub fn new() -> Amazons {
+    let b = Board::new();
     Amazons {
-      current: board.clone(),
-      history: vec![HistoryMove::NewGame(board)],
+      current: b.clone(),
+      history: vec![HistoryMove::NewGame(b)],
       cache: DistState::new(),
     }
   }
@@ -32,7 +35,7 @@ impl Amazons {
         self.current = b;
       }
       Some(HistoryMove::Move(m)) => {
-        self.current.un_apply_move(&m);
+        self.current.un_apply_move(m);
       }
       None => {}
     }
@@ -43,57 +46,13 @@ impl Amazons {
     &self.current()
   }
 
-  /// All the pieces owned by a team.
-  pub fn team_pieces(&self, team: Team) -> Vec<Pos> {
-    self
-      .current
-      .players()
-      .filter(move |p| p.team == team)
-      .map(|p| p.pos)
-      .collect()
-  }
-
   /// Try to record a player's move
   ///
   /// Return Err(msg) explaining the error if the move is invalid.
-  pub fn player_move(&mut self, team: Team, pos: Pos, mv: Pos, shot: Pos) -> Result<(), String> {
-    let board = self.current.clone();
-
-    for &coord in &[pos, mv, shot] {
-      if coord.row >= board.board_size || coord.col >= board.board_size {
-        return Err(format!(
-          "Coord {:?} is outside board_size ({}, {})",
-          coord, board.board_size, board.board_size
-        ));
-      }
-    }
-    if pos == mv || mv == shot || !pos.in_a_line_with(mv) {
-      return Err(format!("Moves not in a line!"));
-    }
-    if !mv.in_a_line_with(shot) {
-      return Err(format!("Shoot is not in a line!"));
-    }
-    if let Some(er) = pos.along_line(mv).iter().find(|&&p| board.wall_at(p)) {
-      return Err(format!("Can't move through piece at {:?}", er));
-    }
-    if let Some(er) = mv.along_line(shot).iter().filter(|&&p| p != pos).find(|&&p| board.wall_at(p)) {
-      return Err(format!("Can't place token through piece at {:?}", er));
-    }
-    if let Some((pi, p)) = board.players().enumerate().find(|(_,play)| play.pos == pos) {
-      if p.team == team {
-        let new_move = CompactMove {
-          player_ix: pi,
-          old_pos: pos,
-          new_pos: mv,
-          new_shot: shot,
-        };
-        self.current.apply_move(&new_move);
-        self.history.push(HistoryMove::Move(new_move));
-        self.history.truncate(100);
-        return Ok(());
-      }
-    }
-    return Err(format!("You don't have a piece at the position"));
+  pub fn player_move(&mut self, mv: Move) {
+    self.current.apply_move(mv);
+    self.history.push(HistoryMove::Move(mv));
+    self.history.truncate(100);
   }
 
   /// Compute and make a move for an AI team.
@@ -114,18 +73,18 @@ impl Amazons {
   }
 }
 
-#[cfg(test)]
-mod tests {
-  use crate::solver::*;
+// #[cfg(test)]
+// mod tests {
+//   use crate::solver::*;
 
-  #[test]
-  fn ai_move() {
-    let b = CompactBoard::new(6, vec![
-      Player{pos:Pos{row:1,col:1},team:Team::Red},
-      Player{pos:Pos{row:2,col:2},team:Team::Blue},
-    ]);
-    let mut a = Amazons::from_board(b);
-    println!("{:?}", a.ai_move(Team::Red));
-    assert_eq!(2 + 2, 4);
-  }
-}
+//   #[test]
+//   fn ai_move() {
+//     let b = CompactBoard::new(6, vec![
+//       Player{pos:Pos{row:1,col:1},team:Team::Red},
+//       Player{pos:Pos{row:2,col:2},team:Team::Blue},
+//     ]);
+//     let mut a = Amazons::from_board(b);
+//     println!("{:?}", a.ai_move(Team::Red));
+//     assert_eq!(2 + 2, 4);
+//   }
+// }
